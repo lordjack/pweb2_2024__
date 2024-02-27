db3842ecd8ba597de1d7e3d294769f441694d316
Jackson Meires <lordjackson@gmail.com>
2024-02-27 16:03:11 -0300

    upload projeto

---
+++ app/Http/Controllers/AlunoController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Aluno;
+use Illuminate\Http\Request;
+
+class AlunoController extends Controller
+{
+    public function index()
+    {
+        return view("aluno.list");
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view("aluno.form");
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //app/http/Controller
+        Aluno::create(
+            [
+                'nome' => $request->nome,
+                'telefone' => $request->telefone,
+                'cpf' => $request->cpf,
+            ]
+        );
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AlunoController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//routes/web.php
+Route::get('/aluno', [AlunoController::class,"index"]);
+//carrega o formulário
+Route::get('/aluno/create', [AlunoController::class,"create"]);
+//recebe os dados do formulario para ser salvo na função store
+Route::post('/aluno', [AlunoController::class,"store"])->name('aluno.store');
+
